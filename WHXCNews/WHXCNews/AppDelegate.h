0f8c2d6743317c16010ee9d803b75009a5ca2506
Dear <330410585@qq.com>
2016-08-19 15:02:14 +0800

    项目初始化

---
+++ WHXCNews/WHXCNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHXCNews
+//
+//  Created by Dear on 16/8/19.
+//  Copyright © 2016年 bear_fighting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
